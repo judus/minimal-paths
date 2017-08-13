ecd163058b262a256ee38fec02f18b1ae7b7e6e3
Julien Duseyau <julien.duseyau@gmail.com>
2017-08-13 18:38:08 +0200

    Preparing tests

--- tests/ExampleTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Maduser\Minimal\Paths\Tests;
-
-class ExampleTest extends \PHPUnit_Framework_TestCase
-{
-
-}
---
+++ tests/PathGeneratorTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Maduser\Minimal\Paths\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class PathGeneratorTest extends TestCase
+{
+
+}
